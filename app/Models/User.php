9bd87dfe55cf31cc58348a84b4bcaf950b4ce530
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-01-10 14:32:53 +0300

    User permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use Laravel\Paddle\Billable;
+use Spatie\Permission\Traits\HasRoles;
 
 
 
@@ -17,6 +18,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     use Billable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
