cf9e00ae97c135af027050476b21ade313226e7e
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-01-11 19:20:51 +0300

    updade name

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,13 +37,13 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'fullName' => 'required|string|max:255',
+            'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
         return User::create([
-            'name' => $request->fullName,
+            'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
