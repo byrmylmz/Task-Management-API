a8179a731f7c6ab3dcaa6caf41b208603209e228
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-02-01 20:33:36 +0300

    fortify installed email verification completed

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\EmailVerificationNotificationController;
+use Laravel\Fortify\Http\Controllers\NewPasswordController;
+use Laravel\Fortify\Http\Controllers\PasswordResetLinkController;
+use Laravel\Fortify\Http\Controllers\VerifyEmailController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -92,4 +96,16 @@ use Illuminate\Support\Facades\Route;
     Route::post('/oauth/token', [PassportController::class,'auth']);
     Route::post('/login', [AuthController::class,'login']);
     Route::post('/register', [AuthController::class,'register']);
+    //Email verification
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth:api','throttle:6,1']);
+    
+    Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+    ->middleware(['signed','throttle:6,1'])->name('verification.verify');
+
+    //Password Forgot.
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.update');
+    Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+
 
