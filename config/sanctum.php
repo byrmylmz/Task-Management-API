a2dceb3b901083a0694f51f63d25bbb553631370
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-02-05 14:40:33 +0300

    update

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        '*.alakod.com,api.alakod.com,sanctum.alakod.com,alakod.com,localhost,localhost:3000,localhost:8080,localhost:8000,127.0.0.1,127.0.0.1:8080,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,alakod.com',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
