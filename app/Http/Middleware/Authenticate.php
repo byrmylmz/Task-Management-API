38d7da6a12dcb236893b3bb91c2b873cd7c02815
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-02-08 09:38:34 +0300

    update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return url(env('SPA_URL') . '/login');
+            return url('https://sanctum.alakod.com/login');
         }
     }
 }
