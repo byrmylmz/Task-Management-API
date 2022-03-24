dfc7fb0250e323970260095f984dd54a6947f356
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-03-24 13:23:03 +0300

    u

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'paddle/*', 
-        'google/webhook',  
+        'google/webhook',
+        'telescope/*',  
     ];
 }
