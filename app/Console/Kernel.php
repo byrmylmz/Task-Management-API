1a0a4331acdf6248b12fb827244ec4300ee1d389
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-06-01 18:24:45 +0300

    App\Console\RefreshWebhookSynchronizations not found

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Jobs\PeriodicSynchronizations;
+use App\Jobs\RefreshWebhookSynchronizations;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
